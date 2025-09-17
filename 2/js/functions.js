9231d72a95fd74bd9aeb70112acda7b6ee82a25f
Keks <keks@htmlacademy.ru>
2025-09-18 00:43:33 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,15 @@
+const checkStringLength = function (string, maxLength) {
+  return string.length <= maxLength;
+};
+
+const isPalindrome = function (string) {
+  const normalizedString = string.replaceAll(' ', '').toUpperCase();
+
+  let reversedString = '';
+
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    reversedString += normalizedString[i];
+  }
+
+  return normalizedString === reversedString;
+};
